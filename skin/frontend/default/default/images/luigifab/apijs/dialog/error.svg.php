fdecfed92ec815b592d1528a58fdbd3328a1dd4c
Fabrice Creuzot <code@luigifab.fr>
2013-03-10 20:00:00 +0200

    Version 4.2.0

--- skin/frontend/default/default/images/luigifab/apijs/dialog/error.svg.php
+++ skin/frontend/default/default/images/luigifab/apijs/dialog/error.svg.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Created L/18/02/2013
- * Updated V/22/02/2013
- * Version 5
+ * Updated M/26/02/2013
+ * Version 6
  *
  * Copyright 2013 | Fabrice Creuzot (luigifab) <code~luigifab~info>
  * http://www.luigifab.info/apijs
@@ -32,15 +32,13 @@ $translations = array();
 $translations['en'] = array('numb' => '404', 'text' => 'Error');
 $translations['fr'] = array('numb' => '404', 'text' => 'Erreur');
 
-if (!array_key_exists($lang, $translations))
-	$lang = 'en';
-
-$opacityStart = ((strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false) || (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) ? 1 : 0;
+$lang = (!array_key_exists($lang, $translations)) ? 'en' : $lang;
+$opacity = ((strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false) || (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) ? 1 : 0;
 
 ?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg width="<?php echo $width ?>" height="<?php echo $height ?>" xmlns="http://www.w3.org/2000/svg" version="1.1">
 	<g transform="rotate(-20, <?php echo intval(intval($width) / 2) ?>, <?php echo intval(intval($height) / 2) ?>)">
-		<g font-family="Verdana, sans-serif" font-weight="bold" fill="black" fill-opacity="<?php echo $opacityStart ?>">
+		<g font-family="Verdana, sans-serif" font-weight="bold" fill="black" fill-opacity="<?php echo $opacity ?>">
 			<text x="38%" y="47%" font-size="<?php echo intval(1.1 * intval($height) / 6) ?>">
 				<tspan><?php echo $translations[$lang]['text'] ?></tspan>
 				<animate attributeName="x" dur="0.4s" begin="0.5s" from="28%" to="38%" />
@@ -51,7 +49,7 @@ $opacityStart = ((strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false) || (st
 			</text>
 			<animate attributeName="fill-opacity" dur="0.6s" begin="0.7s" from="0" to="1" fill="freeze" />
 		</g>
-		<g opacity="<?php echo $opacityStart ?>">
+		<g opacity="<?php echo $opacity ?>">
 			<line x1="10%" y1="37%" x2="11.75%" y2="39%" stroke="black" stroke-width="6%" stroke-linecap="round" />
 			<circle cx="20%" cy="50%" r="16%" fill="black" />
 			<line x1="12.5%" y1="42.5%" x2="27.5%" y2="57.5%" stroke="red" stroke-width="7%" stroke-linecap="round" />
