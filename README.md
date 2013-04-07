ce2c769dc035cdff86f4312620b91ffc672b77ed
Fabrice Creuzot <code@luigifab.fr>
2013-04-07 20:00:00 +0200

    Version 4.2.1

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ For more information, go to https://www.luigifab.fr/apijs and https://www.luigif
 
 This repository is a mirror. To install the module, please use the extension key available in the documentation. If you like, take some of your time to improve the translations, go to https://bit.ly/2HyCCEc.
 
-Version 4.2.0 released on 10/03/2013 (repacked and reminified on 16/02/2020).
+Version 4.2.1 released on 07/04/2013 (repacked and reminified on 16/02/2020).
--- skin/adminhtml/default/default/images/luigifab/apijs/dialog/saving.svg.php
+++ skin/adminhtml/default/default/images/luigifab/apijs/dialog/saving.svg.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Created S/16/02/2013
- * Updated V/08/03/2013
- * Version 6
+ * Updated V/05/04/2013
+ * Version 7
  *
  * Copyright 2013 | Fabrice Creuzot (luigifab) <code~luigifab~info>
  * http://www.luigifab.info/apijs
@@ -31,11 +31,11 @@ $height = (isset($_GET['h'])) ? $_GET['h'].'px' : '100%';
 <svg width="<?php echo $width ?>" height="<?php echo $height ?>" xmlns="http://www.w3.org/2000/svg" version="1.1">
 	<circle cx="50%" cy="50%" r="5%" fill="#222" />
 	<circle cx="50%" cy="50%" r="40%" stroke="#222" stroke-opacity="0" fill="none" stroke-width="10%">
-		<animate attributeName="r" dur="0.9s" begin="0.5s;a.end+0.7s" from="2%" to="40%" id="a" />
-		<animate attributeName="stroke-opacity" dur="0.9s" begin="0.5s;b.end+0.7s" from="1" to="0" id="b" />
+		<animate attributeName="r" dur="0.9s" begin="0.1s;a.end+0.7s" from="2%" to="40%" id="a" />
+		<animate attributeName="stroke-opacity" dur="0.9s" begin="0.1s;b.end+0.7s" from="1" to="0" id="b" />
 	</circle>
 	<circle cx="50%" cy="50%" r="40%" stroke="#222" stroke-opacity="0" fill="none" stroke-width="10%">
-		<animate attributeName="r" dur="0.9s" begin="0.95s;c.end+0.7s" from="2%" to="40%" id="c" />
-		<animate attributeName="stroke-opacity" dur="0.9s" begin="0.95s;d.end+0.7s" from="1" to="0" id="d" />
+		<animate attributeName="r" dur="0.9s" begin="0.55s;c.end+0.7s" from="2%" to="40%" id="c" />
+		<animate attributeName="stroke-opacity" dur="0.9s" begin="0.55s;d.end+0.7s" from="1" to="0" id="d" />
 	</circle>
 </svg>
